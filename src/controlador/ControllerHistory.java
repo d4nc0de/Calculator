db883923187119cdac4064750610288d3259083e
Daniel Oliveros <icloudaniel764@gmail.com>
2024-05-25 15:17:49 -0500

    Cambios en ControllerVista

--- src/controlador/ControllerHistory.java
+++ src/controlador/ControllerHistory.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.DefaultListModel;
+import javax.swing.SwingUtilities;
 import modelo.History;
 import modelo.Modelo;
 import vista.Vista;
@@ -23,17 +24,17 @@ public class ControllerHistory {
     private Modelo model;
     private static Vista view;
 
-    public ControllerHistory(Modelo model, Vista view) {
-        this.view = view;
+    public ControllerHistory(Modelo model) {
+        this.view = view.getInstance();
         this.model = model;
 
     }
 
     public static Response updateHistory() {
         try {
-            DefaultListModel modelolist = new DefaultListModel();
+            DefaultListModel<String> modelolist = new DefaultListModel<>();
             System.out.println("1");
-            
+
             System.out.println("2");
             modelolist.removeAllElements();
             System.out.println("3");
@@ -41,11 +42,15 @@ public class ControllerHistory {
             System.out.println("4");
 
             for (int i = array.size() - 1; i >= 0; i--) {
-                modelolist.addElement(array.get(i));                
+                modelolist.addElement(array.get(i).toString());
             }
             System.out.println("5");
-            view.txtHistory.setModel(modelolist);
+
+            SwingUtilities.invokeLater(() -> {
+                view.txtHistory.setModel(modelolist);
+            });
             System.out.println("6");
+
             return new Response("Person data updated successfully", Status.OK);
         } catch (Exception ex) {
             return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
